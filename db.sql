7b6f04e75bcee3caad3e4585a53b63a4591933cc
MinseopJung <sethmsjung@gmail.com>
2020-11-04 23:54:15 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -48,5 +48,41 @@ select max(file_order) from depth;
 delete from depth where file_order = 309;
 
 
+###################################################################
+
+START TRANSACTION;                    
+	SELECT * FROM my_table;                    
+	DELETE FROM my_table WHERE gender = 'M';  
+	SELECT * FROM my_table;    
+
+ROLLBACK;                               
+	SELECT * FROM my_table;                     
+	START TRANSACTION;                       
+	DELETE FROM my_table WHERE gender = 'M';    
+
+COMMIT;                                    
+	SELECT * FROM my_table;                     
+
+###################################################################
 
 
+SELECT 		# group by, having에서는 select 컬럼의 별칭 사용 가능, 같은 select 컬럼에서 앞 컬럼의 별칭 사용 불가 (sub 쿼리에 where에 사요 가능, but 일반적인 호출은 불가)
+	DATE_FORMAT(A.reg_date, "%Y-%m") AS C_DATE_YM,
+	DATE_FORMAT(A.reg_date + INTERVAL -1 MONTH, "%Y-%m") as P_DATE_YM, 
+    
+    SUM(A.user_base) AS C_SUM_USER_BASE, 
+    (
+        SELECT SUM(B.user_base) FROM test_ori AS B WHERE DATE_FORMAT(B.reg_date, "%Y-%m") = P_DATE_YM
+	) AS P_SUM_USER_BASE,
+    #	C_SUM_VALUE + Error Code: 1054. Unknown column
+    (
+		CONCAT
+        (
+			((SELECT SUM(B.user_base) FROM test_ori AS B WHERE DATE_FORMAT(B.reg_date, "%Y-%m") = P_DATE_YM)  / SUM(A.user_base) - 1) * 100, 
+            "%"
+		)
+	) AS RATIO 
+FROM test_ori AS A 
+#WHERE DATE_FORMAT(A.reg_date, "%Y-%m") >= "2017-04" 
+GROUP BY C_DATE_YM, P_DATE_YM HAVING C_SUM_USER_BASE > 1 ORDER BY C_DATE_YM
+;
